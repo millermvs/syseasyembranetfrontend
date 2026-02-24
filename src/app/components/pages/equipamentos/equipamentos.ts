778bb9ec65f728cf747ecfee6c62d1a4e5a30640
millermvs <millermvs@gmail.com>
2026-02-24 20:46:53 -0300

    feat: adiciona ícones de edição e exclusão na tabela de equipamentos com interações

--- src/app/components/pages/equipamentos/equipamentos.ts
+++ src/app/components/pages/equipamentos/equipamentos.ts
@@ -59,9 +59,9 @@ export class Equipamentos {
     ]]
   });
 
-    pesquisarEquipamento() {
-      console.log("Pesquisar por IP:", this.formPesquisarEquipamento.value.ipPesquisa);
-    }
+  pesquisarEquipamento() {
+    console.log("Pesquisar por IP:", this.formPesquisarEquipamento.value.ipPesquisa);
+  }
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////
   ///////////////////////////////////////////FormilarioAdd/MetodosAdd//////////////////////////////////////
@@ -81,7 +81,7 @@ export class Equipamentos {
     macDoAp: [''],
   });
 
-   adicionarEquipamento() {
+  adicionarEquipamento() {
     this.http.post(`http://localhost:8080/api/v1/equipamentos/cadastrar`, this.formAddEquipamento.value)
       .subscribe({
         next: (response: any) => {
@@ -98,6 +98,155 @@ export class Equipamentos {
         }
       });
   }
+
+  addBuscarInfoViaSNMP() {
+    const ip = this.formAddEquipamento.get('ip')?.value;
+    if (!ip) return;
+
+    this.mapeandoEquipamento.set(true); // começa o loading
+
+    this.http.get<any>(`http://localhost:8080/api/v1/equipamentos/mapear/${ip}`).subscribe({
+      next: (response: any) => {
+        this.mapeandoEquipamento.set(false); // termina o loading
+        // preenche o formulário com o que veio do backend
+        this.formAddEquipamento.patchValue({
+          ip: response.ip ?? ip,
+          mac: response.mac ?? '',
+          nomeRadio: response.nomeRadio ?? '',
+          ssid: response.ssid ?? '',
+          nivelDeSinal: response.nivelDeSinal ?? '',
+          canalRadio: response.canalRadio ?? '',
+          macDoAp: response.macDoAp ?? '',
+        });
+
+        // opcional: mensagens no modal
+        this.mensagemModal.set('Informações SNMP carregadas com sucesso!');
+        this.tipoMensagem.set('success');
+      },
+      error: (error: any) => {
+        this.mapeandoEquipamento.set(false); // termina o loading mesmo se der erro
+        const msg =
+          error?.error?.message ||
+          error?.message ||
+          'Erro ao buscar informações via SNMP.';
+
+        this.mensagemModal.set(msg);
+        this.tipoMensagem.set('danger');
+      }
+    });
+  }
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////FormilarioEditar/MetodosEditar//////////////////////////////////////
+  formEditEquipamento = this.fb.group({
+    ip: [{ value: '', disabled: true }],
+    mac: [''],
+    nomeRadio: [''],
+    ssid: [''],
+    nivelDeSinal: [''],
+    canalRadio: [''],
+    macDoAp: [''],
+  });
+
+  equipamentoSelecionadoParaEditar: any = null;
+
+  @ViewChild('btnCloseEditEquipamento')
+  btnCloseEditEquipamento!: ElementRef<HTMLButtonElement>;
+
+  abrirModalEditarEquipamento(equip: any) {
+    this.equipamentoSelecionadoParaEditar = equip;
+
+    this.formEditEquipamento.reset({
+      ip: equip.ip,
+      mac: equip.mac,
+      nomeRadio: equip.nomeRadio,
+      ssid: equip.ssid,
+      nivelDeSinal: equip.nivelDeSinal,
+      canalRadio: equip.canalRadio,
+      macDoAp: equip.macDoAp,
+    });
+
+    // força o estado inicial como pristine
+    this.formEditEquipamento.markAsPristine();
+    this.formEditEquipamento.markAsUntouched();
+
+    this.mensagemModal.set('');
+  }
+
+  editarEquipamento() {
+    if (!this.equipamentoSelecionadoParaEditar) return;
+
+    const ip = this.equipamentoSelecionadoParaEditar.ip;
+    const id = this.equipamentoSelecionadoParaEditar.id;
+
+    const payload = {
+      ip: ip,
+      mac: this.formEditEquipamento.get('mac')?.value,
+      nomeRadio: this.formEditEquipamento.get('nomeRadio')?.value,
+      ssid: this.formEditEquipamento.get('ssid')?.value,
+      nivelDeSinal: this.formEditEquipamento.get('nivelDeSinal')?.value,
+      canalRadio: this.formEditEquipamento.get('canalRadio')?.value,
+      macDoAp: this.formEditEquipamento.get('macDoAp')?.value,
+    };
+
+    this.http.put(`http://localhost:8080/api/v1/equipamentos/editar?id=${id}`, payload).subscribe({
+      next: () => {
+        this.mensagemPagPrincipal.set("Equipamento " + ip + " editado com sucesso!");
+        this.tipoMensagem.set("success");
+        this.consultarEquipamentos(this.paginaAtual());
+        this.btnCloseEditEquipamento.nativeElement.click();
+        setTimeout(() => this.mensagemPagPrincipal.set(''), 4000);
+        this.equipamentoSelecionadoParaEditar = null;
+      },
+      error: (error: any) => {
+        this.mensagemModal.set(error?.error?.message || "Erro ao editar equipamento.");
+        this.tipoMensagem.set("danger");
+      }
+    });
+  }
+
+  editBuscarInfoViaSNMP() {
+    if (!confirm(`Ao consultar via SNMP os dados serão salvos automaticamente. Tem certeza que deseja buscar informações via SNMP para o equipamento?`)) {
+      return;
+    }
+    const ip = this.formEditEquipamento.get('ip')?.value;
+    if (!ip) return;
+
+    this.mapeandoEquipamento.set(true); // começa o loading
+
+    this.http.get<any>(`http://localhost:8080/api/v1/equipamentos/mapear/${ip}`).subscribe({
+      next: (response: any) => {
+        this.mapeandoEquipamento.set(false); // termina o loading
+        // preenche o formulário com o que veio do backend
+        this.formEditEquipamento.patchValue({
+          ip: response.ip ?? ip,
+          mac: response.mac ?? '',
+          nomeRadio: response.nomeRadio ?? '',
+          ssid: response.ssid ?? '',
+          nivelDeSinal: response.nivelDeSinal ?? '',
+          canalRadio: response.canalRadio ?? '',
+          macDoAp: response.macDoAp ?? '',
+        });
+
+        // opcional: mensagens no modal
+        this.mensagemModal.set('Informações SNMP carregadas e salvas com sucesso!');
+        this.tipoMensagem.set('success');
+        setTimeout(() => this.mensagemModal.set(''), 3000);
+      },
+      error: (error: any) => {
+        this.mapeandoEquipamento.set(false); // termina o loading mesmo se der erro
+        const msg =
+          error?.error?.message ||
+          error?.message ||
+          'Erro ao buscar informações via SNMP.';
+
+        this.mensagemModal.set(msg);
+        this.tipoMensagem.set('danger');
+      }
+    });
+  }
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////ConsultarEquipamento///////////////////////////////////////////////
 
   consultarEquipamentos(pagina = 0) {
     const url = `http://localhost:8080/api/v1/equipamentos/listar?page=${pagina}&size=${this.pageSize}`;
@@ -137,43 +286,16 @@ export class Equipamentos {
     }
   }
 
- 
 
-  buscarInfoViaSNMP() {
-    const ip = this.formAddEquipamento.get('ip')?.value;
-    if (!ip) return;
 
-    this.mapeandoEquipamento.set(true); // começa o loading
 
-    this.http.get<any>(`http://localhost:8080/api/v1/equipamentos/mapear/${ip}`).subscribe({
-      next: (response: any) => {
-        this.mapeandoEquipamento.set(false); // termina o loading
-        // preenche o formulário com o que veio do backend
-        this.formAddEquipamento.patchValue({
-          ip: response.ip ?? ip,
-          mac: response.mac ?? '',
-          nomeRadio: response.nomeRadio ?? '',
-          ssid: response.ssid ?? '',
-          nivelDeSinal: response.nivelDeSinal ?? '',
-          canalRadio: response.canalRadio ?? '',
-          macDoAp: response.macDoAp ?? '',
-        });
 
-        // opcional: mensagens no modal (seguindo seu padrão)
-        this.mensagemModal.set('Informações SNMP carregadas com sucesso!');
-        this.tipoMensagem.set('success');
-      },
-      error: (error: any) => {
-        this.mapeandoEquipamento.set(false); // termina o loading mesmo se der erro
-        const msg =
-          error?.error?.message ||
-          error?.message ||
-          'Erro ao buscar informações via SNMP.';
 
-        this.mensagemModal.set(msg);
-        this.tipoMensagem.set('danger');
-      }
-    });
+  deletarEquipamento(equipamento: any) {
+    if (!confirm(`Tem certeza que deseja deletar o equipamento ${equipamento.ip}?`)) {
+      return;
+    }
+    console.log("Deletar equipamento:", equipamento);
   }
 
 }
